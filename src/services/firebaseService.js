1e794a076bd8b5fd27ef82f694dc2acacac8697f
DaliaZahran <dalia_zhran@hotmail.com>
2024-01-05 15:56:18 +0200

    fix database config error

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -10,7 +10,6 @@ import { collection, getDocs } from "firebase/firestore";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 import firebaseConfig from "../constants/env";
 
-console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
