db7a6f9a80c9ce6bcb2fe5e8ba964b07e4eed510
DaliaZahran <dalia_zhran@hotmail.com>
2024-01-05 17:30:46 +0200

    Update sign in screen styles

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,88 +1,136 @@
 // SignInScreen.js
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text, Button, StyleSheet } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { signIn } from '../redux/actions/authActions'; // Update with the correct path
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'; // Import specific auth functions
-
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Button,
+  StyleSheet,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import { signIn } from "../redux/actions/authActions"; // Update with the correct path
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; // Import specific auth functions
 
 const SignInScreen = ({ navigation }) => {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignIn = async () => {
     try {
+      // Basic validation
+      if (!email || !password) {
+        console.error("Invalid input");
+        return;
+      }
 
-        // Basic validation
-        if (!email || !password) {
-            console.error('Invalid input');
-            return;
-        }
+      // Get the auth instance
+      const auth = getAuth();
 
-        // Get the auth instance
-        const auth = getAuth();
+      // Sign in the user with the provided email and password
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-        // Sign in the user with the provided email and password
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      // Dispatch the signIn action with the user data
+      dispatch(signIn(userCredential.user));
 
-        // Dispatch the signIn action with the user data
-        dispatch(signIn(userCredential.user));
-    
-        // Optionally, you can perform additional actions after sign-in, such as navigating to the home screen
-        navigation.navigate('Home');
+      // Optionally, you can perform additional actions after sign-in, such as navigating to the home screen
+      navigation.navigate("Home");
 
-        console.log('User signed in successfully!');
+      console.log("User signed in successfully!");
     } catch (error) {
-        console.error('Error signing in:', error);
+      console.error("Error signing in:", error);
     }
   };
 
-    // Function to navigate to the SignIn screen
-    const navigateToSignUp = () => {
-        navigation.navigate('SignUp');
-    };
+  // Function to navigate to the SignIn screen
+  const navigateToSignUp = () => {
+    navigation.navigate("SignUp");
+  };
 
   return (
     <View style={styles.container}>
-      {/* Your UI components for sign-in */}
+      <Text style={styles.titleText}>تسجيل الدخول</Text>
       <TextInput
-        placeholder="Email"
+        placeholder="البريد الإلكتروني"
         value={email}
         onChangeText={(text) => setEmail(text)}
         style={styles.input}
       />
       <TextInput
-        placeholder="Password"
+        placeholder="كلمة السر"
         value={password}
         onChangeText={(text) => setPassword(text)}
         secureTextEntry
         style={styles.input}
       />
-      <Button title="Sign In" onPress={handleSignIn} />
+      <TouchableOpacity onPress={handleSignIn} style={styles.signInButton}>
+        <Text style={styles.signInButtonText}>تسجيل الدخول</Text>
+      </TouchableOpacity>
       <TouchableOpacity onPress={navigateToSignUp}>
-        <Text style={styles.signUpButton}>You don't have an account? Sign Up</Text>
+        <Text style={styles.signUpButtonText}>
+          ليس لديك حساب؟
+          <Text style={styles.signUpButtonSubtext}> تسجيل حساب جديد</Text>
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
 
+const commonSignUpTextStyle = {
+  fontSize: 15,
+  fontStyle: "normal",
+  fontWeight: "400",
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 16,
+  },
+  titleText: {
+    fontSize: 24,
+    textAlign: "right",
+    margin: 16,
   },
   input: {
     marginBottom: 12,
-    padding: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 10,
     borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+    textAlign: "right",
   },
-  signUpButton: {
-    marginTop: 16,
-    color: 'blue', // You can customize the color
-    textAlign: 'center',
-    textDecorationLine: 'underline',
+  signInButton: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+    backgroundColor: "#9A98FF",
+    marginTop: 4,
+    marginBottom: 12,
+  },
+  signInButtonText: {
+    fontSize: 16,
+    color: "#fff",
+    textAlign: "center",
+  },
+  signUpButtonText: {
+    ...commonSignUpTextStyle,
+    color: "#333",
+    textAlign: "center",
+  },
+  signUpButtonSubtext: {
+    ...commonSignUpTextStyle,
+    color: "#6E85E3",
   },
 });
 
